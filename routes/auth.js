077412b2b9a8d3038eeff81324f405aed29266e4
Murarimohansai <murarimohansai@gmail.com>
2026-01-16 17:41:19 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ msg: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Wrong password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
