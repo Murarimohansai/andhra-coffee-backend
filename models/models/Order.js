d1cf725078e977314f931a021cb2ffe6703a3e44
Murarimohansai <murarimohansai@gmail.com>
2026-01-16 17:16:43 +0530

    Create Order model with schema definition

---
+++ models/models/Order.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  items: Array,
+  total: Number,
+  paymentMethod: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
